d680edf4676ed02acd211b7cb1d81d93a29e230e
Alex Moran <alexander.moran99@gmail.com>
2020-06-03 10:43:34 -0400

    Work with spam set and logistic regression.

--- Working Script.R
+++ Working Script.R
@@ -13,3 +13,22 @@ coef(reg_interact)
 #triple interaction!
 ojreg <- glm(log(sales)~ log(price)*brand*feat, data=oj)
 coef(ojreg)
+
+
+#working with emails and spam, for logistic regression practice:
+email <- read.csv("spam.csv")
+email[c(1,4000), c(16,56,58)]
+spammy <- glm(spam ~., data=email,family='binomial')
+coef(spammy)["word_free"]
+exp(1.542)
+# so we see that if an email contains the word "free"
+# it is nearly 5x more likely to be spam
+coef(spammy)["word_george"]
+1/exp(-5.779)
+# on the other hand, emails which contain the word "george"
+# are over 300x less likely to be spam
+
+# let's use the model to predict!
+predict(spammy,newdata=email[c(1,4000),])
+# to transform to probabilities:
+predict(spammy, newdata=email[c(1,4000),], type='response')
